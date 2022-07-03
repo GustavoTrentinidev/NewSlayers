624b433e4bcba1af987946d736157f9e094f0fa1
Gustavo Henrique Trentini <gustavotrentini22@gmail.com>
2022-07-02 23:03:50 -0300

    footer

---
+++ src/components/FooterDefault.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="footer">
+    <div class="box-conteudo">
+        <div class="box-linha">
+            <img src="@/assets/logo.png" class="logo" alt="">
+            <h1>NewSlayers</h1>
+        </div>
+        <h1 class="texto">Notícias sobre a Riot Games</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.footer{
+    margin-top: 100px;
+    width: 100%;
+    height: 240px;
+    position: relative;
+    bottom: 0;
+    background-color: #14112C;
+    border-top: 2px solid #fff;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #fff;
+}
+.box-conteudo{
+    width: 440px;
+    height: 90px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+
+}
+.box-linha{
+    display: flex;
+    gap: 20px;
+    margin: 0 auto;
+    position: relative;
+    top: 30px;
+}
+.box-linha h1{
+    font-weight: normal;
+}
+.box-conteudo .texto{
+    position: relative;
+    top: -30px;
+    font-weight: normal;
+}
+.logo{
+  position: relative;
+  width: 60px;
+  height: 100px;
+  top: -15px;
+  left: 15px;
+}
+</style>
--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -2,13 +2,16 @@
     <div>
         <Header/>
         <router-view></router-view>
+        <Footer/>
     </div>
 </template>
 
 <script>
 import Header from '../components/HeaderDefault.vue'
+import Footer from '../components/FooterDefault.vue'
+
 export default {
-    components: {Header}
+    components: {Header, Footer}
 }
 </script>
 
