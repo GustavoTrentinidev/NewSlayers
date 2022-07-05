f737957c51b19d3704dd03d61f90b1fc9f664f53
GustavoTrentinidev <gustavotrentini22@gmail.com>
2022-07-05 15:10:29 -0300

    começo tela de login

--- src/components/MelhoresAutores.vue
+++ src/components/MelhoresAutores.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="autores">
       <div class="autor" v-for="(autor, index) in melhoresAutores" :key="index">
-            <carousel-3d display="1" width="360" height="480" :disable3d="true" border="0" :autoplay="true" :autoplayTimeout="5000" :autoplayHoverPause="true">
+            <carousel-3d :ref="mycarousel" display="1" width="360" height="480" :disable3d="true" border="0" :autoplay="true" :autoplayTimeout="5000" :autoplayHoverPause="true">
                 <slide class="slide" :style="'background-image: url('+ noticia.img + ')'" :index="index" v-for="(noticia, index) in autor.noticiasMaisCurtidas" :key="index">
                     <div class="carrossel-botoes">
-                        <div class="botao" v-for="(noticia, index) in autor.noticiasMaisCurtidas" :key="index" @click="goToSlide()"></div>
+                        <div class="botao" v-for="(noticia, index) in autor.noticiasMaisCurtidas" :key="index" @click="goToSlide($event,index)"></div>
                     </div>
                 </slide>
             </carousel-3d>
@@ -24,8 +24,9 @@ export default {
         Slide,
   },
   methods:{
-    goToSlide(){
-        console.log(this.$refs)
+    goToSlide(e,index){
+        console.log(e.target.parentNode.parentNode.parentNode.parentNode,index)
+        console.log(this.$refs.mycarousel)
     }
   },
     data(){
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,5 +1,17 @@
 <template>
-    <div class="main"></div>
+  <div class="main">
+      <div class="esquerda"></div>
+      <div class="direita">
+          <div class="conteudo">
+              <div class="forms">
+                  <label for="login">Login</label>
+                <input class="input" placeholder="Login" name="login" type="text">
+                    <label for="login">Senha</label>
+                <input class="input" placeholder="Senha" name="senha" type="text">
+              </div>
+          </div>
+      </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +21,46 @@ export default {
 </script>
 
 <style scoped>
-
+.main {
+  display: flex;
+}
+.esquerda{
+    width: 60%;
+    height: 100vh;
+    background-color: blue;
+}
+.direita{
+    width: 40%;
+    height: 100vh;
+    background-color: brown;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.conteudo{
+    display: flex;
+    flex-direction: column;
+    width: 600px;
+    height: 500px;
+    background-color: #2D265F;
+    border-radius: 10px;
+}
+.forms{
+    position:relative;
+    margin: 0 auto;
+    top: 70px;
+    display: flex;
+    flex-direction: column;
+}
+.conteudo input{
+    width: 540px;
+    height: 75px;
+    outline: 0;
+    border-radius: 8px;
+    border: 0;
+    background-color: #14112C;
+    color: #fff;
+    font-size: 30px;
+    padding: 0px 10px 0 10px;
+}
 </style>
